ab5787705043c1e3634d79b330ccd150003bfc53
Dan Marino <technicaldanny302@gmail.com>
2024-09-29 16:22:09 -0700

    completed exercise

---
+++ LINQ/VideoGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class VideoGame
+    {
+        public static void Games()
+        {
+            List<string> allGames = new List<string>() { "HALO", "COD", "MARIO" };
+
+            var games = allGames.OrderBy(game => game.Length);
+
+            foreach (var game in games)
+            {
+                Console.WriteLine(game); 
+            }
+        }
+        
+
+    }
+}
